b0079918363b5200f13e2afea944598537aacaf4
idaselma3025 <idaselma3025@gmail.com>
2018-01-17 18:18:01 -0600

    fjfj

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -24,15 +24,16 @@ $("#add-train-btn").on("click", function(event) {
   var destination = $("#destination-input").val().trim();
   var trainStart = $("#start-input").val().trim();
   var frequency = $("#frequency-input").val().trim();
+  // var currentTime = firebase.database.ServerValue.TIMESTAMP;
 
-  var next = "place holder";
 
   newTrain = {
     trainName: trainName,
     destination: destination,
     trainStart: trainStart,
     frequency: frequency,
-    key:true
+    key:true,
+    currentTime:firebase.database.ServerValue.TIMESTAMP
   };
 
 
@@ -49,24 +50,27 @@ $("#add-train-btn").on("click", function(event) {
 
 database.ref().orderByKey().on("child_added", function(childSnapshot, prevChildKey) {
 
-  var trainName = childSnapshot.val().trainName;
+  var trainName = childSnapshot.val().childtrainName;
   var destination = childSnapshot.val().destination;
   var trainStartDb = childSnapshot.val().trainStart;
   var tFrequency = childSnapshot.val().frequency;
   var keyDb = childSnapshot.key;
+  var currentTime = childSnapshot.val().currentTime;
+  console.log(currentTime);
 
   var trainStartConverted = moment(trainStartDb,"HH:mm A").subtract(1,"years");
   // console.log("converted : " + trainStartConverted);
-  var currentTime = moment();
-  // console.log("CURRENT TIME: "+ moment(currentTime).format("HH:mm A"));
-  var diffTime = moment().diff(moment(trainStartConverted),"minutes");
-  // console.log("DIFFERENCE IN TIME : "+diffTime);
+  // var currentTimeConverted = moment(currentTime).format("HH:mm A");
+  console.log("CURRENT TIME: "+ moment(currentTime).format("HH:mm A"));
+  var diffTime = moment(currentTime).diff(moment(trainStartConverted),"minutes");
+  console.log("DIFFERENCE IN TIME : "+diffTime);
   var tRemainder = diffTime % tFrequency;
   // console.log(tRemainder);
   var tMinutesTrain = tFrequency - tRemainder;
-  // console.log("MINUTES TILL TRAIN : " +tMinutesTrain);
+  console.log("MINUTES TILL TRAIN : " +tMinutesTrain);
   var nextTrain = moment().add(tMinutesTrain, "minutes");
   var arrivalTime =moment(nextTrain).format("HH:mm A");
+  console.log("arrivalTime : "+arrivalTime);
 
   var newTr=$("<tr>");
   newTr.attr("id",keyDb);
@@ -78,7 +82,7 @@ database.ref().orderByKey().on("child_added", function(childSnapshot, prevChildK
   newTr.append(nameTd,destTd,freqTd,arrivalTd,minutesTd);
   $("#schedule-table > tbody").append(newTr);
 });
-
+//
 var number = 10;
 var intervalId;
 function run(){
@@ -87,16 +91,35 @@ function run(){
 function decrement(){
   number --;
   if (number === 0){
+
+    number =10;
     console.log("decrement is working");
+database.ref().orderByKey().on("child_added",function(snapshot){
+  var newTime = firebase.database.ServerValue.TIMESTAMP;
+  keykey = snapshot.key;
+  console.log("key : "+keykey);
+  database.ref().child(keykey).update({currentTime:newTime});
+  database.ref().child(keykey).on("value",function(childSnapshot){
+    var trainStartDb = childSnapshot.val().trainStart;
+    var tFrequency = childSnapshot.val().frequency;
+    var currentTime = childSnapshot.val().currentTime;
+    var trainStartConverted = moment(trainStartDb,"HH:mm A").subtract(1,"years");
+    // console.log("converted : " + trainStartConverted);
+    // var currentTimeConverted = moment(currentTime).format("HH:mm A");
+    console.log("CURRENT TIME: "+ moment(currentTime).format("HH:mm A"));
+    var diffTime = moment(currentTime).diff(moment(trainStartConverted),"minutes");
+    console.log("DIFFERENCE IN TIME : "+diffTime);
+    var tRemainder = diffTime % tFrequency;
+    // console.log(tRemainder);
+    var tMinutesTrain = tFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN : " +tMinutesTrain);
+    var nextTrain = moment().add(tMinutesTrain, "minutes");
+    var arrivalTime =moment(nextTrain).format("HH:mm A");
+    console.log("arrivalTime : "+arrivalTime);
+  })
 
-  }
+})
+  // console.log(snap);
+}
 };
-
-userConnected.on("value",function(snapshot){
-  console.log(snapshot.val());
-  if(snapshot.val()){
-    run();
-    console.log("userConnected is working");
-  }
-});
-console.log(number);
+ run();
